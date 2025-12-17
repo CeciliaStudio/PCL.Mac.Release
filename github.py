3f7e799ebc36ab7a468f540698ec57cce8fa000d
AnemoFlower <yizhimcqiu@icloud.com>
2025-12-17 15:58:26 +0800

    feat: GitHub Release 上传

--- github.py
+++ github.py
@@ -15,13 +15,17 @@ class Artifact:
     name: str
     download_url: str
 
+@dataclass
+class Release:
+    html_url: str
+    upload_url: str
+    id: int
+
 API_ROOT = "https://api.github.com"
 
-def get_last_workflow_run(repo: str, token: Optional[str]) -> WorkflowRun:
+def get_last_workflow_run(repo: str, token: str) -> WorkflowRun:
     url: str = API_ROOT + f"/repos/{repo}/actions/runs"
-    headers: dict[str, str] = {}
-    if token is not None:
-        headers["Authorization"] = f"Bearer {token}"
+    headers: dict[str, str] = {"Authorization": f"Bearer {token}"}
     response: requests.Response = requests.get(url, headers=headers, verify=False)
     workflow_run: dict[str, Any] = response.json()["workflow_runs"][0]
     return WorkflowRun(id=workflow_run["id"],
@@ -29,10 +33,38 @@ def get_last_workflow_run(repo: str, token: Optional[str]) -> WorkflowRun:
                        display_title=workflow_run["display_title"],
                        created_at=workflow_run["created_at"])
 
-def get_workflow_artifacts(repo: str, id: int, token: Optional[str]) -> list[dict[str, Any]]:
+def get_workflow_artifacts(repo: str, id: int, token: str) -> list[dict[str, Any]]:
     url: str = API_ROOT + f"/repos/{repo}/actions/runs/{id}/artifacts"
-    headers: dict[str, str] = {}
-    if token is not None:
-        headers["Authorization"] = f"Bearer {token}"
+    headers: dict[str, str] = {"Authorization": f"Bearer {token}"}
     response: requests.Response = requests.get(url, headers=headers, verify=False)
     return response.json()["artifacts"]
+
+def generate_release_notes(repo: str, tag_name: str, token: str) -> str:
+    url: str = API_ROOT + f"/repos/{repo}/releases/generate-notes"
+    headers: dict[str, str] = {"Authorization": f"Bearer {token}"}
+    body: dict[str, str] = {"tag_name": tag_name}
+    return requests.post(url, headers=headers, json=body, verify=False).json()["body"]
+
+def create_release(repo: str, tag_name: str, name: str, notes: str, token: str) -> Release:
+    url: str = API_ROOT + f"/repos/{repo}/releases"
+    headers: dict[str, str] = {"Authorization": f"Bearer {token}"}
+    body: dict[str, str] = {
+        "tag_name": tag_name,
+        "name": name,
+        "body": notes
+    }
+    response = requests.post(url, headers=headers, json=body, verify=False)
+    response.raise_for_status()
+    release: dict[str, Any] = response.json()
+    return Release(html_url=release["html_url"],
+                   upload_url=release["upload_url"],
+                   id=release["id"])
+
+def upload_release_asset(upload_url: str, filename: str, path: str, token: str) -> None:
+    url: str = upload_url.replace("{?name,label}", f"?name={filename}")
+    headers: dict[str, str] = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/octet-stream"
+    }
+    with open(path, "rb") as file:
+        requests.post(url, headers=headers, data=file, verify=False).raise_for_status()
--- main.py
+++ main.py
@@ -31,7 +31,28 @@ def download_artifact(url: str, name: str, digest: str) -> None:
     print("\n工件下载超时")
     exit(1)
 
+def generate_release_notes(tag_name: str, description: str, workflow_run_id: int) -> str:
+    raw_release_notes: str = github.generate_release_notes(repo=GITHUB_REPOSITORY, tag_name=tag_name, token=GITHUB_TOKEN)
+    lines: list[str] = raw_release_notes.split("\n")
+    pull_requests: list[str] = [line for line in lines[1:lines.index("")] if "[NOTE-IGN]" not in line]
+    lines = [
+        description,
+        "",
+        "## 更新日志",
+        "",
+        *pull_requests,
+        "",
+        f"完整日志：{lines[-1].split(": ")[-1]}",
+        "",
+        "---",
+        "",
+        f"本版本为 https://github.com/{GITHUB_REPOSITORY}/actions/runs/{workflow_run_id}"
+    ]
+    return "\n".join(lines)
+
 def main() -> None:
+    if not os.path.exists("run"):
+        os.mkdir("run")
     os.chdir("run")
     print(f"PCL.Mac Release CLI {VERSION}")
 
@@ -50,9 +71,38 @@ def main() -> None:
         i += 1
     i = int(input("选择一个工件："))
     artifact: dict[str, Any] = artifacts[i - 1]
-    artifact_url: str = f"https://nightly.link/CeciliaStudio/PCL.Mac.Refactor/actions/runs/{id}/{artifact["name"]}.zip"
+    artifact_url: str = f"https://nightly.link/{GITHUB_REPOSITORY}/actions/runs/{id}/{artifact["name"]}.zip"
     print(f"下载 URL：{artifact_url}")
     download_artifact(url=artifact_url, name="artifact.zip", digest=artifact["digest"])
+
+    release_title: str = input("输入 Release 版本号：")
+    tag_name: str = input("输入 Release tag：")
+    description: str = input("输入 Release 描述：")
+    print("正在生成更新日志...")
+    release_notes = generate_release_notes(tag_name=tag_name, description=description, workflow_run_id=id)
+    with open("release-notes.md", mode="w+", encoding="utf-8") as file:
+        file.write(release_notes)
+    print("将在 1s 后打开更新日志")
+    time.sleep(1)
+    os.system("open release-notes.md")
+    print("请编辑更新日志，随后按下 Enter")
+    input("> ")
+    with open("release-notes.md", mode="r+", encoding="utf-8") as file:
+        release_notes = file.read()
+    
+    print("正在创建 GitHub Release...")
+    release = github.create_release(repo=GITHUB_REPOSITORY,
+                                    tag_name=tag_name,
+                                    name=release_title,
+                                    notes=release_notes,
+                                    token=GITHUB_TOKEN)
+    print("正在上传工件...")
+    github.upload_release_asset(upload_url=release.upload_url,
+                                filename=f"PCL.Mac.Refactor-{tag_name}.zip",
+                                path="artifact.zip",
+                                token=GITHUB_TOKEN)
+    print("工件上传成功")
+    print(f"GitHub Release 已创建：{release.html_url}")
 
 if __name__ == "__main__":
     main()
